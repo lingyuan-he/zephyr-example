25549df31c622c691d740dcd8a6c1e9297ac385e
Lingyuan He <lingyuan.he@hotmail.com>
2025-02-14 23:05:23 -0800

    Overlay format fix and remove dead code

--- app/src/main.c
+++ app/src/main.c
@@ -15,78 +15,13 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 #define MIN_GRAVITY_ACCEL 9
 
-// !!
-
-#define ROW1_PORT_BIT 21
-#define ROW2_PORT_BIT 22
-#define ROW3_PORT_BIT 15
-#define ROW4_PORT_BIT 24
-#define ROW5_PORT_BIT 19
-  
-#define COL1_PORT_BIT 28
-#define COL2_PORT_BIT 11
-#define COL3_PORT_BIT 31
-#define COL4_PORT_BIT 5
-#define COL5_PORT_BIT 30
-  
-static const struct device *gpio0, *gpio1;
-int matrix_begin()
-{
-    int ret;
-    // Configure the GPIO's     
-    gpio0=DEVICE_DT_GET(DT_NODELABEL(gpio0));      //device_get_binding("GPIO_0");
-    if (gpio0 == NULL)
-    {
-        printf("Error acquiring GPIO 0 interface\n");
-        return -1;
-    }
-    gpio1=DEVICE_DT_GET(DT_NODELABEL(gpio1)); //device_get_binding("GPIO_1");
-    if (gpio0 == NULL)
-    {
-        printf("Error acquiring GPIO 1 interface\n");
-        return -2;
-    }
-    ret = gpio_pin_configure(gpio0,ROW1_PORT_BIT,GPIO_OUTPUT);
-    ret = gpio_pin_configure(gpio0,ROW2_PORT_BIT,GPIO_OUTPUT);
-    ret = gpio_pin_configure(gpio0,ROW3_PORT_BIT,GPIO_OUTPUT);
-    ret = gpio_pin_configure(gpio0,ROW4_PORT_BIT,GPIO_OUTPUT);
-    ret = gpio_pin_configure(gpio0,ROW5_PORT_BIT,GPIO_OUTPUT);
-      
-    ret = gpio_pin_configure(gpio0,COL1_PORT_BIT,GPIO_OUTPUT);
-    ret = gpio_pin_configure(gpio0,COL2_PORT_BIT,GPIO_OUTPUT);
-    ret = gpio_pin_configure(gpio0,COL3_PORT_BIT,GPIO_OUTPUT);
-    ret = gpio_pin_configure(gpio1,COL4_PORT_BIT,GPIO_OUTPUT);
-    ret = gpio_pin_configure(gpio0,COL5_PORT_BIT,GPIO_OUTPUT);
-
-    ret = gpio_pin_set(gpio0,ROW1_PORT_BIT,1); 
-    ret = gpio_pin_set(gpio0,ROW2_PORT_BIT,1);
-    ret = gpio_pin_set(gpio0,ROW3_PORT_BIT,1);
-    ret = gpio_pin_set(gpio0,ROW4_PORT_BIT,1);
-    ret = gpio_pin_set(gpio0,ROW5_PORT_BIT,0);
-    ret = gpio_pin_set(gpio0,COL1_PORT_BIT,0);
-    ret = gpio_pin_set(gpio0,COL2_PORT_BIT,0);
-    ret = gpio_pin_set(gpio0,COL3_PORT_BIT,1);
-    ret = gpio_pin_set(gpio1,COL4_PORT_BIT,0);
-    ret = gpio_pin_set(gpio0,COL5_PORT_BIT,0);
-  
-    LOG_INF("End of matrix");
-    return 0;   
-}
-
-
-// !!
-
-
 int main(void)
 {
     int rc;
-	const struct device *accel, *ledmatrix;
+    const struct device *accel, *ledmatrix;
     struct accel_values accel_xyz;
 
-	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
-
-    // !!
-    //matrix_begin();
+    printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
 
     accel = get_accel_device();
     if (accel == NULL) {
@@ -102,8 +37,6 @@ int main(void)
         LOG_ERR("Failed to get the LED matrix device");
         return -1;
     }
-
-    LOG_ERR("ready %d", device_is_ready(ledmatrix));
 
 	while (1) {
         rc = get_accel_values(accel, &accel_xyz);
@@ -143,9 +76,9 @@ int main(void)
             ledmatrix_turn_off(ledmatrix);
         }
 
-		k_sleep(K_SECONDS(1));
-	}
+        k_sleep(K_SECONDS(1));
+    }
 
-	return 0;
+    return 0;
 }
 
